34ef3ecde57a6a3549b7d417cd73ab73d8fa1a2e
evagolabek <golabek.eva@gmail.com>
2018-10-07 17:35:59 +0200

    var let const

--- app.js
+++ app.js
@@ -1,33 +1,81 @@
-alert('hello');
-console.log('Hello World');
-console.log(123);
-console.log(true);
-var greeting = 'hello';
+// alert('hello');
+// console.log('Hello World');
+// console.log(123);
+// console.log(true);
+// var greeting = 'hello';
 
-console.log(greeting);
-//prints an array, individual index and value
-console.log([1,2,3,4]); 
-//object litteral, prints the object
-console.log({a:1, b:2});
+// console.log(greeting);
+// //prints an array, individual index and value
+// console.log([1,2,3,4]); 
+// //object litteral, prints the object
+// console.log({a:1, b:2});
 
-//prints a table
-console.table({a:1, b:2});
+// //prints a table
+// console.table({a:1, b:2});
 
-//red error
-console.log('This is some error');
+// //red error
+// console.log('This is some error');
 
-console.clear();
+// console.clear();
 
-//yellow error
-console.log('This is a warning!');
+// //yellow error
+// console.log('This is a warning!');
 
-//to know how long something takes in your script
-console.time('Hello');
-  console.log('Hello World');
-  console.log('Hello World');
-  console.log('Hello World');
-  console.log('Hello World');
-  console.log('Hello World');
-  console.log('Hello World');
-console.timeEnd('Hello');
+// //to know how long something takes in your script
+// console.time('Hello');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+// console.timeEnd('Hello');
 
+//var name = 'John Doe';
+//reassign of variables
+//name ='Steve Smith';
+
+//initialize var, will return undefined
+//var greeting;
+//console.log(greeting);
+
+//assinging smth to the var
+//greeting = 'hello';
+
+//VARIABLES: letter, numbers, _, $
+//cannot start with a number
+//multiword camelCase, under_score,PascalCase
+
+//LET
+//let name;
+//name = 'John Doe';
+//console.log(name);
+//name = 'Steve Smith';
+//console.log(name);
+
+//CONST
+//const name = 'John';
+//console.log(name); //cannot reassign
+//have to assign a value
+//const greeting;
+
+//you can change stuff in here but not reassign person
+//even if you reassign data inside the object it is still assigned to that object
+// const person = {
+//   name: 'John',
+//   age: 30
+// }
+
+// person.name = 'Sara';
+
+// console.log(person);
+
+//you can change arrays and object that are assigned to const
+//we cannot redeclare it, like changing to a whole new array
+// const numbers = [1,2,3,4,5];
+// numbers.push(6);
+
+// console.log(numbers);
+
+//const let your program/whoever use it know that it should not be reassigned
+//you can mutate it if its array or an object, but it cannot be reassigned as a primitive value
